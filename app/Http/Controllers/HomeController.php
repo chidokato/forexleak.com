2dc0d95ee8ad0bd777230538113e07d96770a4aa
chidokato <tuan.pn92@gmail.com>
2025-06-21 12:33:46 +0700

    sdf

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -44,7 +44,7 @@ class HomeController extends Controller
         $slider = Slider::orderBy('id', 'desc')->get();
         $product = Post::where('hot', 'true')->where('sort_by', 'Product')->orderBy('id', 'desc')->take(8)->get();
         $posts = Post::where('sort_by', 'News')->orderBy('id', 'desc')->take(5)->get();
-        $brokers = Post::where('sort_by', 'page')->orderBy('id', 'desc')->take(5)->get();
+        $brokers = Section::where('post_id', 730)->orderBy('id', 'desc')->take(5)->get();
 
         return view('pages.home', compact(
             'slider',
