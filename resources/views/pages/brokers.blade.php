40d779c9e6a00b814bf1b99d7b428dfbfd85a634
chidokato <tuan.pn92@gmail.com>
2025-06-21 10:03:31 +0700

    sdf

--- resources/views/pages/brokers.blade.php
+++ resources/views/pages/brokers.blade.php
@@ -52,33 +52,34 @@
         </div>
         
         @foreach($data->Section as $val)
-        <div class="host-web-price-item bg-white p-4 pt-30 rounded-3 mt-20 position-relative z-1">
+        <div class="host-web-price-item bg-white p-3 rounded-3 mt-20 position-relative z-1">
             <!-- <div class="sale-badge text-center position-absolute z--1">
                 <span class="text-white fw-bold">30% Sale</span>
             </div> -->
             <div class="row align-items-center gap-4 gap-xl-0">
-                <div class="col-xl-5">
+                <div class="col-xl-3">
                     <div class="d-flex align-items-center flex-wrap flex-sm-nowrap gap-7">
                         <div class="host-web-op-bg d-inline-block rounded-3 a1212">
                             <img src="https://www.forexcracked.com/wp-content/uploads/elementor/thumbs/exness-logo-ForexCracked.com_-e1743009751580-r3frzh605c3tcvdh8j7xa9xnw0bxaj65r7gm4iujgg.png" alt="" class="img-fluid">
+                            <span class="fs-20">{{ $val->heading }}</span>
                         </div>
                         <div class="host-web-content">
-                            <h5 class="text-black fs-24 mb-10">{{ $val->heading }}</h5>
-                            <p class="mb-0">Exness is a globally trusted forex broker known for ultra-fast execution, low trading costs, and instant 24/7 withdrawals!</p>
+                            <!-- <h5 class="text-black fs-24 mb-10">{{ $val->heading }}</h5> -->
+                            <!-- <p class="mb-0">Exness is a globally trusted forex broker known for ultra-fast execution, low trading costs, and instant 24/7 withdrawals!</p> -->
                         </div>
                     </div>
                 </div>
-                <div class="col-xl-7">
+                <div class="col-xl-9">
                     <div class="row align-items-center">
-                        <div class="col-md-9">
+                        <div class="col-md-9 info-list">
                             {!! $val->content !!}
                         </div>
                         <div class="col-md-3">
-                            <div class="pl-40 mb-3">
+                            <div class="mb-2">
                                 <a href="" class="template-btn isb-small-btn isb-gd-bg rounded-4 rounded-pill btn-small a111">Register</a>
                             </div>
-                            <div class="pl-40">
-                                <a href="" class="template-btn isb-small-btn isb-gd-bg rounded-4 rounded-pill btn-small a111">Review</a>
+                            <div class="">
+                                <a href="" class="template-btn outline-btn rounded-4 rounded-pill btn-small a111">Review</a>
                             </div>
                         </div>
                     </div>
