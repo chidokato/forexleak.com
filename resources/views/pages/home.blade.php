674c935e9cb31b7eddd5bc32aa156175206ab4f7
chidokato <tuan.pn92@gmail.com>
2025-11-09 17:27:25 +0700

    update

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -17,7 +17,7 @@
     <div class="container">
         <div class="row justify-content-center">
             <div class="col-lg-10 col-xxl-8">
-                <div class="text-center mt-4">
+                <!-- <div class="text-center mt-4">
                     <h1 class="text-white fs-64">$25K in Funded Trading Accounts Giveaway</h1>
                 </div>
                 <ul class="list-unstyled d-flex align-items-center justify-content-center gap-3 flex-wrap mb-0 mt-4">
@@ -37,7 +37,7 @@
                 <div class="d-flex align-items-center justify-content-center gap-3 flex-wrap pt-40">
                     <a href="" class="template-btn host-fs-btn-bg fs-hover-style fw-bold text-black rounded-pill">Enter Now!</a>
                     <a href="" class="template-btn outline-btn fs-outline-hover-style text-warning host-fs-border-clr rounded-pill">Contact !</a>
-                </div>
+                </div> -->
             </div>
         </div>
     </div>
